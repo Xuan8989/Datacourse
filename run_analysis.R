a23d8b2d274ae53b9caeaefbf9d1ad653891c36a
Xuan Lin <linxuan93@icloud.com>
2023-04-15 00:16:08 -0400

    unzip the file

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,4 @@
-
+# download the zip file pertaining the data collected through Samsung Galacy S smartphone
+if(!file.exists("./data")){dir.create("./data")}
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl, destfile = "./data/smartphone.zip")
